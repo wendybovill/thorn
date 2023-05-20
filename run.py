5bd8758fb1fb535d6b5d5094cb44c37b55442c04
wendybovill <wendybovill@gmail.com>
2023-05-20 15:47:47 +0000

    update before deployment

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
-#decorator @app wrapping function in /
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -23,14 +26,18 @@ def about():
 def about_member(member_name):
     member = {}
     with open("data/company.json", "r") as json_data:
-        data= json.load(json_data)
+        data = json.load(json_data)
         for obj in data:
             if obj["url"] == member_name:
                 member = obj
     return render_template("member.html", member=member)
 
-@app.route("/contact")
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message.".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -39,10 +46,8 @@ def careers():
     return render_template("careers.html", page_title="Careers")
 
 
-# __main__ the main default module in python
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
-
